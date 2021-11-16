3510c14b8ba6ee6a3da59635a478ff1835a96953
nicholasbcarr <carrn7@hawaii.edu>
2021-11-16 10:27:31 -1000

    finished landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Grid, Icon, Header, Comment } from 'semantic-ui-react';
+import { Grid, Icon, Header, Comment, Image } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    const commentStyle = { color: 'white' };
     return (
       <div className="kanak-attack-landing-background">
-        <Grid container centered stackable columns={3}>
+        <Grid container centered stackable columns={4}>
 
           <Grid.Column textAlign="center">
             <Icon size='huge' name="utensils" inverted/>
@@ -29,6 +30,15 @@ class Landing extends React.Component {
             <Header as='h3' inverted>Stay alerted with new menu items and new specials from your
               favorite vendors. Review and rate your favorite vendors.</Header>
           </Grid.Column>
+
+          <Grid.Column textAlign="center">
+            <Icon size='huge' name="lightbulb outline" inverted/>
+            <Header as='h1' inverted>Kanak Attack History</Header>
+            <Header as='h3' inverted>This slang term, with the shortened ”Kanak” from ”Kanaka”
+              may have been coined from the resulting effect one feels after eating all the
+              “onolicious” (delicious) food offered at a traditional Hawaiian Luau.</Header>
+          </Grid.Column>
+
         </Grid>
         <div>
           <Grid container centered>
@@ -37,16 +47,26 @@ class Landing extends React.Component {
                 <Comment>
                   <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
                   <Comment.Content>
-                    <Comment.Author>Phillip Johnson (MD)</Comment.Author>
-                    <Comment.Text>
-                    Hoooo brah! I cannot wait for more kanaks!!!
+                    <Comment.Author style={commentStyle}>Phillip Johnson (MD)</Comment.Author>
+                    <Comment.Metadata>
+                      <div style={commentStyle}>2 days ago</div>
+                      <div>
+                        <Icon style={commentStyle} name='star' color="white" />
+                        <Icon style={commentStyle} name='star' color="white" />
+                        <Icon style={commentStyle} name='star' color="white" />
+                        <Icon style={commentStyle} name='star' color="white" />
+                        <Icon style={commentStyle} name='star' color="white" />
+                      </div>
+                    </Comment.Metadata>
+                    <Comment.Text style={commentStyle}>
+                      Hoooo brah! I cannot wait for more kanaks!!!
                     </Comment.Text>
                   </Comment.Content>
                 </Comment>
               </Comment.Group>
             </Grid.Row>
             <Grid.Row>
-              <img src="images/kanak-definition.png"/>
+              <Image src='https://pbs.twimg.com/profile_images/1052001602628857856/AGtSZNoO_400x400.jpg' size='medium' />
             </Grid.Row>
           </Grid>
         </div>
