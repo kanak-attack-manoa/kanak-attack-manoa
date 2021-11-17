ba6bdedebcee30c30fd0221c58d56c3669b39f8d
triciamarier <reyestr@hawaii.edu>
2021-11-17 11:07:46 -1000

    updated background and layout

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -11,9 +11,9 @@ class NavBar extends React.Component {
   render() {
     const menuStyle = { marginBottom: '10px' };
     return (
-      <Menu style={menuStyle} attached="top" borderless inverted>
+      <Menu style={menuStyle} attached="top" borderless>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
-          <Header inverted as='h1'>Kanak Attack Manoa</Header>
+          <Header as='h1'>Kanak Attack Manoa</Header>
         </Menu.Item>
         {this.props.currentUser ? (
           [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,47 +5,53 @@ import { Grid, Icon, Header, Comment, Image } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     const commentStyle = { color: 'white' };
+    const imageStyle = { height: '200px', padding: '10px 0px 0px 0px' };
+    const reviewStyle = { padding: '40px 0px 0px 0px' };
     return (
       <div className="kanak-attack-landing-background">
-        <Grid container centered stackable columns={4}>
+        <div>
+          <Grid container centered>
+            <Grid.Row>
+              <Image src='https://www6.pbrc.hawaii.edu/logos/manoaseal_transparent.png' size='small' />
+            </Grid.Row>
+            <Grid.Row textAlign="center">
+              <Header as='h1' inverted style={ { fontSize: '40px' } }>Welcome to Kanak Attack Manoa</Header>
+              <Header as='h3' inverted>The slang term, Kanak Attack, with the shortened ”Kanak” from ”Kanaka”
+                may have been coined from the resulting effect one feels after eating all the
+                “onolicious” (delicious) food offered at a traditional Hawaiian Luau.</Header>
+            </Grid.Row>
+          </Grid>
+        </div>
+
+        <Grid container centered stackable columns={3} style={reviewStyle}>
 
           <Grid.Column textAlign="center">
             <Icon size='huge' name="utensils" inverted/>
             <Header as='h1' inverted>Vendors</Header>
-            <Header as='h3' inverted>Vendors can update their specials, menu items,
+            <Header as='h4' inverted>Vendors at the UH Manoa Campus can update their specials, menu items,
               menu item ingredients and much much more!!</Header>
           </Grid.Column>
 
           <Grid.Column textAlign="center">
             <Icon size='huge' name="users" inverted/>
             <Header as='h1' inverted>Users</Header>
-            <Header as='h3' inverted>Users will have the ability to search for different
+            <Header as='h4' inverted>Users have the ability to search for different
               types of cuisine, ingredients or dietary choices. We hope to align our users
               with the best local choice for their kanak attack needs.</Header>
           </Grid.Column>
 
           <Grid.Column textAlign="center">
             <Icon size='huge' name="bullhorn" inverted/>
-            <Header as='h1' inverted>Never miss a Kanak Attack</Header>
-            <Header as='h3' inverted>Stay alerted with new menu items and new specials from your
+            <Header as='h1' inverted>Never Miss a Kanak Attack</Header>
+            <Header as='h4' inverted>Stay alerted with new menu items and new specials from your
               favorite vendors. Review and rate your favorite vendors.</Header>
           </Grid.Column>
 
-          <Grid.Column textAlign="center">
-            <Icon size='huge' name="lightbulb outline" inverted/>
-            <Header as='h1' inverted>Kanak Attack History</Header>
-            <Header as='h3' inverted>This slang term, with the shortened ”Kanak” from ”Kanaka”
-              may have been coined from the resulting effect one feels after eating all the
-              “onolicious” (delicious) food offered at a traditional Hawaiian Luau.</Header>
-          </Grid.Column>
-
-        </Grid>
-        <div>
-          <Grid container centered>
+          <Grid container centered style={reviewStyle}>
             <Grid.Row>
-              <Comment.Group className="landing-comments" size='massive' >
+              <Comment.Group size='large' >
                 <Comment>
-                  <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+                  <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/steve.jpg' />
                   <Comment.Content>
                     <Comment.Author style={commentStyle}>Phillip Johnson (MD)</Comment.Author>
                     <Comment.Metadata>
@@ -65,10 +71,10 @@ class Landing extends React.Component {
                 </Comment>
               </Comment.Group>
             </Grid.Row>
-            <Grid.Row>
-              <Image src='https://pbs.twimg.com/profile_images/1052001602628857856/AGtSZNoO_400x400.jpg' size='medium' />
-            </Grid.Row>
           </Grid>
+        </Grid>
+        <div>
+          <Image fluid src='images/food.png' style={imageStyle}/>
         </div>
       </div>
     );
