416a21ef77a3e1e4bf06bf75f2a66395eaeec616
LumD2000 <89867005+LumD2000@users.noreply.github.com>
2021-11-18 09:43:12 -1000

    Created default values for Vendors

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
-
+import { Vendors } from '../../api/vendor/Vendor';
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
@@ -14,5 +14,13 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+// Initialize the StuffsCollection if empty.
+if (Vendors.collection.find().count() === 0) {
+  if (Meteor.settings.defaultVendors) {
+    console.log('Creating default Vendors.');
+    Meteor.settings.defaultVendors.map(data => addData(data));
   }
 }
