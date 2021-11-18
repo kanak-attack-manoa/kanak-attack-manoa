05e66d0c5560bee5aad0feadf1d244cb6ff36667
nicholasbcarr <carrn7@hawaii.edu>
2021-11-17 16:10:07 -1000

    AddMenuItem, Schema, Navbar Edit, Setting.json to include default menu items

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -16,7 +16,7 @@ class NavBar extends React.Component {
           <Header as='h1'>Kanak Attack Manoa</Header>
         </Menu.Item>
         {this.props.currentUser ? (
-          [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
+          [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Menu Item</Menu.Item>,
             <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
