e257dd578a8a93e80882d769e3248a0d550f836f
triciamarier <reyestr@hawaii.edu>
2021-12-06 20:14:31 -1000

    edit vendor profile

---
+++ app/imports/ui/pages/VendorHome.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Grid, Loader, Header, Segment } from 'semantic-ui-react';
+import swal from 'sweetalert';
+import { AutoForm, ErrorsField, HiddenField, LongTextField, SubmitField, TextField } from 'uniforms-semantic';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { Vendors } from '../../api/vendor/Vendor';
+
+const bridge = new SimpleSchema2Bridge(Vendors.schema);
+
+/** Renders the Page for editing a single document. */
+class EditVendorProfile extends React.Component {
+
+  // On successful submit, insert the data.
+  submit(data) {
+    const { name, image, description, _id } = data;
+    const owner = Meteor.user().username;
+    Vendors.collection.update(_id, { $set: { name, image, description, owner } }, (error) => (error ?
+      swal('Error', error.message, 'error') :
+      swal('Success', 'Item updated successfully', 'success')));
+  }
+
+  // If the subscription(s) have been received, render the page, otherwise show a loading icon.
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  renderPage() {
+    return (
+      <Grid container centered>
+        <Grid.Column>
+          <Header as="h2" textAlign="center" inverted>Edit Your Vendor Profile</Header>
+          <AutoForm schema={bridge} onSubmit={data => this.submit(data)} model={this.props.doc}>
+            <Segment>
+              <TextField name='name'/>
+              <LongTextField name='description'/>
+              <TextField name='image'/>
+              <SubmitField value='Submit'/>
+              <ErrorsField/>
+              <HiddenField name='owner' />
+            </Segment>
+          </AutoForm>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+// Require the presence of a Stuff document in the props object. Uniforms adds 'model' to the props, which we use.
+EditVendorProfile.propTypes = {
+  doc: PropTypes.object,
+  model: PropTypes.object,
+  ready: PropTypes.bool.isRequired,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+export default withTracker(({ match }) => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const documentId = match.params._id;
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(Vendors.userPublicationName);
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the document
+  const doc = Vendors.collection.findOne(documentId);
+  return {
+    doc,
+    ready,
+  };
+})(EditVendorProfile);
