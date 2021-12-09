4a2f626d661ccd3eb27b6f34c86a06e08e9f2105
nicholasbcarr <carrn7@hawaii.edu>
2021-12-08 19:02:59 -1000

    more wait times

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -8,9 +8,9 @@ class LandingPage {
 
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
-    const waitTime = 25;
+    const waitTime = 50;
     console.log(`Waiting ${waitTime} seconds before running LandingPage.isDisplayed().`);
-    await testController.wait(waitTime * 10000).expect(this.pageSelector.exists).ok();
+    await testController.wait(waitTime * 1000).expect(this.pageSelector.exists).ok();
   }
 }
 
